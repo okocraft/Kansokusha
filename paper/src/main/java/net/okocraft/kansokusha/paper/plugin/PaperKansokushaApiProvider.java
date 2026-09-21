74da27cf0194959e6810ad2f70e6d2aa26b0a459
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2026-09-21 23:46:45 +0900

    Add internal Paper API provider

---
+++ paper/src/main/java/net/okocraft/kansokusha/paper/plugin/PaperKansokushaApiProvider.java
@@ -0,0 +1,35 @@
+package net.okocraft.kansokusha.paper.plugin;
+
+import net.okocraft.kansokusha.api.KansokushaApi;
+import net.okocraft.kansokusha.api.spi.KansokushaApiProvider;
+import org.jetbrains.annotations.ApiStatus;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+
+@ApiStatus.Internal
+public final class PaperKansokushaApiProvider implements KansokushaApiProvider {
+
+    private static final AtomicReference<KansokushaApi> PUBLISHED = new AtomicReference<>();
+
+    static void publish(KansokushaApi api) {
+        Objects.requireNonNull(api, "api");
+        if (!PUBLISHED.compareAndSet(null, api)) {
+            throw new IllegalStateException("Kansokusha API is already published.");
+        }
+    }
+
+    static void unpublish(KansokushaApi api) {
+        Objects.requireNonNull(api, "api");
+        PUBLISHED.compareAndSet(api, null);
+    }
+
+    @Override
+    public KansokushaApi api() {
+        var api = PUBLISHED.get();
+        if (api == null) {
+            throw new IllegalStateException("Kansokusha API is not currently published.");
+        }
+        return api;
+    }
+}
