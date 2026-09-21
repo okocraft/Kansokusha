cf68a3288932da483a3220f89c1fa1562c5a30fd
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2026-09-22 02:22:10 +0900

    Bootstrap Minecraft registries in Paper codec tests

--- paper/src/test/java/net/okocraft/kansokusha/paper/builtin/PaperBlockStatePayloadCodecTest.java
+++ paper/src/test/java/net/okocraft/kansokusha/paper/builtin/PaperBlockStatePayloadCodecTest.java
@@ -1,15 +1,25 @@
 package net.okocraft.kansokusha.paper.builtin;
 
+import net.minecraft.SharedConstants;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.server.Bootstrap;
 import net.minecraft.world.level.block.CakeBlock;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.Blocks;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class PaperBlockStatePayloadCodecTest {
+
+    @BeforeAll
+    static void bootstrapMinecraft() {
+        SharedConstants.tryDetectVersion();
+        Bootstrap.bootStrap();
+        Bootstrap.validate();
+    }
 
     @Test
     void testBlockBreakPayloadUsesMinecraftBlockStateNbt() throws Exception {
