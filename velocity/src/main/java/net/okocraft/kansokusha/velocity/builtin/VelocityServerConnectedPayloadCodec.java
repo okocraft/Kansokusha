0cffc7e8b6d38a7814e8b28aeed30f0c040e30f4
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2026-09-26 18:54:00 +0900

    Remove unused payload stream imports

--- velocity/src/main/java/net/okocraft/kansokusha/velocity/builtin/VelocityServerConnectedPayloadCodec.java
+++ velocity/src/main/java/net/okocraft/kansokusha/velocity/builtin/VelocityServerConnectedPayloadCodec.java
@@ -6,7 +6,6 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
